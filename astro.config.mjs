df718ffb8ccef29cd74247f3e213a320ac716813
Shogo Kitada <contact@shogo0x2e.com>
2026-08-06 12:09:21 +0900

    Improve profile SEO metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://shogo0x2e.com",
   output: "static",
   trailingSlash: "always",
+  integrations: [sitemap()],
 });
